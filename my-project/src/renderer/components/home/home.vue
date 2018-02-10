498c759172a6030bb0d1bcc4849bf6691429c1ed
谷博文 <d_gubowen@163.com>
2018-02-10 15:49:05 +0800

    Initial commit

---
+++ my-project/src/renderer/components/home/home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="home">{{home}}</div>
+</template>
+<script>
+    export default {
+        name: 'home',
+        data(){
+            return {
+                home: "这是我的首页！"
+            }
+        },
+        mounted() {
+        },
+        methods: {}
+    }
+</script>
+<style lang="css" rel="stylesheet/css">
+    #home{
+        background: #fff;
+    }
+</style>
---
+++ my-project/src/renderer/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import home from '@/components/home/home'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'landing-page',
+            component: require('@/components/LandingPage').default
+        },
+        {
+            path: '/home',
+            name: 'home',
+            component:home
+        },
+        {
+            path: '*',
+            redirect: '/'
+        }
+    ],
+    hashbang: false
+})
